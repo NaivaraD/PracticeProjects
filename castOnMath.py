d7f1ca0360ade9ff71e989047a4ce37784a9bf07
NaivaraD <88801936+NaivaraD@users.noreply.github.com>
2025-03-14 03:33:27 -0700

    Now in EXE! :D
    I have made a GUI for the Cast On Math program, and made it able to be run without fiddling around with python! I'm quite proud as it took longer than it should have lol.

--- castOnMath.py
+++ castOnMath.py
@@ -1,18 +1,123 @@
-import sys
+import tkinter as tk
+from tkinter import ttk
+import os.path
 
-while True:
-    multiple = int(input("What is your multiple: "))
-    nMultRepeat = int(input("What is your buffer number: "))
-    border = int(input("How many total stitches in your border: "))
-    repeats = int(input("How many repeats might you want: "))
+image = 'yarrrn.ico'
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, image)
+
+# Functions
+def theMath(multiple, nMultRepeat, border, repeats):
     increase = multiple
 
     for i in range(repeats-1):
         multiple = multiple+increase
 
-    print("You want to cast on/chain " + str(multiple+nMultRepeat+border) + " stitches!")
+    return(multiple+nMultRepeat+border)
 
-    response = input("Would you like exit? Y/N: ").upper()
+# Build the window
+window = tk.Tk()
+window.title('Cast On Math')
+window.iconbitmap(default = path)
+window_width = 450
+window_height = 200
+display_width = window.winfo_screenwidth()
+display_height = window.winfo_screenheight()
+left = int(display_width / 2 - window_width / 2)
+top = int(display_height / 2 - window_height / 2)
+window.geometry(f'{window_width}x{window_height}+{left}+{top}')
+window.minsize(450, 200)
 
-    if response == "Y":
-        sys.exit()
+# Title Label
+title = ttk.Label(
+    window,
+    text = 'Cast On Math',
+    font = 'Calibri 24'
+    )
+title.pack()
+
+# Multiples
+multipleInt = tk.IntVar(value = 1)
+multipleFrame = ttk.Frame(window)
+multipleLabel = ttk.Label(
+    multipleFrame,
+    text = 'What is your multiple?',
+    font = 'Calibri 12'
+    )
+multiplespin = ttk.Spinbox(
+    multipleFrame,
+    from_ = 1,
+    to = 50,
+    textvariable = multipleInt
+    )
+multipleLabel.pack(side = 'left', padx = 5)
+multiplespin.pack(side = 'left', padx = 5)
+multipleFrame.pack()
+
+# Non-Multiple Repeats
+nMultInt = tk.IntVar(value = 1)
+nMultFrame = ttk.Frame(window)
+nMultLabel = ttk.Label(
+    nMultFrame,
+    text = 'What is your buffer number/turning chain?',
+    font = 'Calibri 12'
+    )
+nMultspin = ttk.Spinbox(
+    nMultFrame,
+    from_ = 1,
+    to = 50,
+    textvariable = nMultInt
+    )
+nMultLabel.pack(side = 'left', padx = 5)
+nMultspin.pack(side = 'left', padx = 5)
+nMultFrame.pack()
+
+# Border
+borderInt = tk.IntVar(value = 1)
+borderFrame = ttk.Frame(window)
+borderLabel = ttk.Label(
+    borderFrame,
+    text = 'How many total stitches in your border?',
+    font = 'Calibri 12'
+    )
+borderspin = ttk.Spinbox(
+    borderFrame,
+    from_ = 1,
+    to = 50,
+    textvariable = borderInt
+    )
+borderLabel.pack(side = 'left', padx = 5)
+borderspin.pack(side = 'left', padx = 5)
+borderFrame.pack()
+
+# Repeats
+repeatInt = tk.IntVar(value = 1)
+repeatFrame = ttk.Frame(window)
+repeatLabel = ttk.Label(
+    repeatFrame,
+    text = 'How many repeats might you want?',
+    font = 'Calibri 12'
+    )
+repeatspin = ttk.Spinbox(
+    repeatFrame,
+    from_ = 1,
+    to = 100,
+    textvariable = repeatInt
+    )
+repeatLabel.pack(side = 'left', padx = 5)
+repeatspin.pack(side = 'left', padx = 5)
+repeatFrame.pack()
+
+# General Output
+mathButton = ttk.Button(
+    window,
+    text = 'Do the Math!',
+    command = lambda: outputLabel.configure(text = f'You want to cast on/chain {theMath(multipleInt.get(), nMultInt.get(), borderInt.get(), repeatInt.get())}')
+    )
+mathButton.pack()
+#outputFrame = ttk.Frame(window, relief = GROOVE)
+outputLabel = ttk.Label(window, text = 'Change the Numbers and Press the Button!', font = 'Calibri 17', relief = tk.GROOVE)
+outputLabel.pack()
+#outputFrame.pack()
+
+window.mainloop()
